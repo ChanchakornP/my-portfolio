5339d226c73ea4822713602b8413d8e7651118ab
ChanchakornP <gamechanchakorn@gmail.com>
2024-12-16 01:41:46 +1100

    update projects

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,11 +27,10 @@ function Header() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/about">About Me</Nav.Link>
                         <Nav.Link href="/resume">Resume</Nav.Link>
-                        <NavDropdown title="Projects" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Project 1</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="/project">Projects</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,50 @@
-
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import machineTranslation from "../Assets/projects/mt-nlp.png"
+import ollama from "../Assets/projects/ollama.png"
+import inkball from "../Assets/projects/inkball.png"
 
 function Projects() {
-
+    return (
+        <Container fluid className="project-section">
+            <Container>
+                <h1 className="project-heading">
+                    My Recent <strong>Projects </strong>
+                </h1>
+                <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={ollama}
+                            isBlog={false}
+                            title="Chat Assistant"
+                            description="Developed ChatGPT-like website that has an ollama as a backend. The hardest part is how to stream the response on the website using flask. The project aims to develop the vector database to create a RAG system."
+                            ghLink="https://github.com/ChanchakornP/Chat"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={inkball}
+                            isBlog={false}
+                            title="Inkball Game"
+                            description="An Inkball game developed in Java and built with Gradle, designed following object-oriented-programming (OOP) principles."
+                            ghLink="https://github.com/ChanchakornP/myInkball"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ProjectCard
+                            imgPath={machineTranslation}
+                            isBlog={false}
+                            title="Machine Translation TH-EN"
+                            description="A transformer model translates Thai sentences to English, and vice versa. The model is trained with open source data such as Thai Wiki, Open Corpus, and Ted Talk. The model is available on Huggingface."
+                            ghLink="https://github.com/ChanchakornP/Machine-Translation-Flask-App"
+                            demoLink="https://huggingface.co/ChanP/finetuned-th-to-en"
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    );
 }
 
 export default Projects;
