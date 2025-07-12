19ef21a1da8a60dc2a601e402f89f1d0dc75a9d2
work <kamech@Kame.local>
2025-07-12 22:33:00 +0700

    Remove .next directory from repository

--- .next/static/chunks/app_page_tsx_ad666826._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_@heroui_dom-animation_dist_index_mjs_4bfd01f8._.js",
-  "static/chunks/node_modules_0e97cfdb._.js",
-  "static/chunks/_b0f7064c._.js"
-],
-    source: "dynamic"
-});
